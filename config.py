f95e17d77b77fa9e0f6135e4c250add33bd40296
Roman Novatorov <roman.novatorov@gmail.com>
2020-02-08 22:58:01 +0300

    config: fix chat members

--- config.py
+++ config.py
@@ -7,5 +7,5 @@ TRIOGRAM_LOGGING_LEVEL = os.getenv("TRIOGRAM_LOGGING_LEVEL", "INFO")
 BOT_LOGGING_LEVEL = os.getenv("BOT_LOGGING_LEVEL", "INFO")
 
 ROUND_DURATION = 10
-CHAT_MEMBERS = os.environ["CHAT_MEMBERS"].split(",")
+CHAT_MEMBERS = list(map(int, os.environ["CHAT_MEMBERS"].split(",")))
 PACK = os.environ["PACK"]
