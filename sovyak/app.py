07a6e543b6cda19a781ea796362bec508d640e52
Roman Novatorov <roman.novatorov@gmail.com>
2020-02-09 00:31:21 +0300

    fix logs

--- sovyak/app.py
+++ sovyak/app.py
@@ -6,10 +6,7 @@ from . import handlers, logs
 
 def make_app(config):
     bot = triogram.make_bot(config.TOKEN)
-
-    logs.configure_triogram(config.TRIOGRAM_LOGGING_LEVEL)
-    logs.configure_bot(config.BOT_LOGGING_LEVEL)
-
+    logs.configure(config)
     new_game_handler = handlers.NewGame(bot, config)
 
     async def app():
--- sovyak/logs.py
+++ sovyak/logs.py
@@ -1,6 +1,24 @@
 import logging
 
 
+def configure(config):
+    configure_sovyak(config.LOGGING_LEVEL)
+    configure_triogram(config.TRIOGRAM_LOGGING_LEVEL)
+
+
+def configure_sovyak(level):
+    logger = logging.getLogger("sovyak")
+    logger.setLevel(level)
+
+    handler = logging.StreamHandler()
+    handler.setLevel(level)
+
+    formatter = logging.Formatter("%(asctime)s %(name)s %(levelname)s: %(message)s")
+    handler.setFormatter(formatter)
+
+    logger.addHandler(handler)
+
+
 def configure_triogram(level):
     logger = logging.getLogger("triogram")
     logger.setLevel(level)
@@ -9,19 +27,6 @@ def configure_triogram(level):
     handler.setLevel(level)
 
     formatter = logging.Formatter("%(asctime)s %(name)s %(request_id)s %(message)s")
-    handler.setFormatter(formatter)
-
-    logger.addHandler(handler)
-
-
-def configure_bot(level):
-    logger = logging.getLogger("bot")
-    logger.setLevel(level)
-
-    handler = logging.StreamHandler()
-    handler.setLevel(level)
-
-    formatter = logging.Formatter("%(asctime)s %(name)s %(levelname)s: %(message)s")
     handler.setFormatter(formatter)
 
     logger.addHandler(handler)
