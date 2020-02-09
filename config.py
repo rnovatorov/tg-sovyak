514e767fb74607dec8ca5acd61f942a34e1e3e3f
Roman Novatorov <roman.novatorov@gmail.com>
2020-02-09 17:45:47 +0300

    config: round duration from env

--- config.py
+++ config.py
@@ -6,6 +6,6 @@ TOKEN = os.environ["TOKEN"]
 LOG_LEVEL = os.getenv("LOG_LEVEL", "INFO")
 TRIOGRAM_LOG_LEVEL = os.getenv("TRIOGRAM_LOG_LEVEL", "WARNING")
 
-ROUND_DURATION = 10
+ROUND_DURATION = int(os.getenv("ROUND_DURATION", 10))
 CHAT_MEMBERS = list(map(int, os.environ["CHAT_MEMBERS"].split(",")))
 PACK = os.environ["PACK"]
