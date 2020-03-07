c9b20f75622dbdf1899629519d562d5604167624
Roman Novatorov <roman.novatorov@gmail.com>
2020-03-07 17:51:49 +0300

    logs: common formatter

--- sovyak/logs.py
+++ sovyak/logs.py
@@ -2,31 +2,26 @@ import logging
 
 
 def configure(config):
-    configure_sovyak(config.LOG_LEVEL)
-    configure_triogram(config.TRIOGRAM_LOG_LEVEL)
+    formatter = logging.Formatter("%(asctime)s %(name)s %(levelname)s: %(message)s")
+    configure_sovyak(config.LOG_LEVEL, formatter)
+    configure_triogram(config.TRIOGRAM_LOG_LEVEL, formatter)
 
 
-def configure_sovyak(level):
-    logger = logging.getLogger("sovyak")
-    logger.setLevel(level)
-
+def configure_sovyak(level, formatter):
     handler = logging.StreamHandler()
     handler.setLevel(level)
-
-    formatter = logging.Formatter("%(asctime)s %(name)s %(levelname)s: %(message)s")
     handler.setFormatter(formatter)
 
+    logger = logging.getLogger("sovyak")
+    logger.setLevel(level)
     logger.addHandler(handler)
 
 
-def configure_triogram(level):
-    logger = logging.getLogger("triogram")
-    logger.setLevel(level)
-
+def configure_triogram(level, formatter):
     handler = logging.StreamHandler()
     handler.setLevel(level)
-
-    formatter = logging.Formatter("%(asctime)s %(name)s %(levelname)s: %(message)s")
     handler.setFormatter(formatter)
 
+    logger = logging.getLogger("triogram")
+    logger.setLevel(level)
     logger.addHandler(handler)
