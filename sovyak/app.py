ae76548821b9f360e09a20bf7323f6f39bb4b400
Roman Novatorov <roman.novatorov@gmail.com>
2020-03-15 13:55:21 +0300

    simplify logging config

--- sovyak/app.py
+++ sovyak/app.py
@@ -1,12 +1,14 @@
+import logging
+
 import trio
 import triogram
 
-from . import handlers, logs
+from . import handlers
 
 
 def make_app(config):
     bot = triogram.make_bot(config.TOKEN)
-    logs.configure(config)
+    configure_logging(config)
     new_game_handler = handlers.NewGame(bot, config)
 
     async def app():
@@ -15,3 +17,19 @@ def make_app(config):
             nursery.start_soon(new_game_handler)
 
     return app
+
+
+def configure_logging(config):
+    handler = logging.StreamHandler()
+    handler.setLevel(logging.DEBUG)
+
+    formatter = logging.Formatter("%(asctime)s %(name)s %(levelname)s: %(message)s")
+    handler.setFormatter(formatter)
+
+    logger = logging.getLogger("sovyak")
+    logger.setLevel(config.LOG_LEVEL)
+    logger.addHandler(handler)
+
+    logger = logging.getLogger("triogram")
+    logger.setLevel(config.TRIOGRAM_LOG_LEVEL)
+    logger.addHandler(handler)
